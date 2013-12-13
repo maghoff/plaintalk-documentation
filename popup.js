43cb71d240d6216950f18dbc15c20128893a9982
Magnus Hoff <maghoff@gmail.com>
2013-12-13 18:13:33 +0100

    Position popups within viewport

--- popup.js
+++ popup.js
@@ -8,7 +8,16 @@ function createPopup(text) {
 	var element = document.createElement("span");
 	element.className = "popup";
 	element.textContent = text;
+	var arrow = document.createElement("span");
+	arrow.className = "popup-arrow";
+	element.appendChild(arrow);
 	return element;
+}
+
+function calculateLeftRelativeToContent(element) {
+	if (!element.offsetParent) return 0;
+	console.log(element.offsetLeft, element);
+	return calculateLeftRelativeToContent(element.offsetParent) + element.offsetLeft;
 }
 
 function installPopup(element) {
@@ -26,10 +35,27 @@ function installPopup(element) {
 
 	popup.style.display = "none";
 	popup.classList.add("suppress-popup");
+	var arrow = popup.querySelector(".popup-arrow");
 	var pendingHideEventId = null;
 	element.addEventListener("mouseover", function () {
+		var x = calculateLeftRelativeToContent(popup);
 		popup.style.display = "";
+
 		setTimeout(function () {
+			var boundingRect = popup.getBoundingClientRect();
+			console.log(boundingRect);
+
+			var diff = null;
+			if (boundingRect.left < 20) {
+				var diff = 20 - boundingRect.left;
+			} else if (boundingRect.right > window.innerWidth - 20) {
+				var diff = -20 + (window.innerWidth - boundingRect.right);
+			}
+			if (diff) {
+				popup.style.left = "calc(50% + " + diff + "px)";
+				arrow.style.left = 150 - diff + "px";
+			}
+
 			popup.classList.remove("suppress-popup");
 		}, 0);
 		if (pendingHideEventId !== null) {
@@ -41,6 +67,8 @@ function installPopup(element) {
 		popup.classList.add("suppress-popup");
 		pendingHideEventId = setTimeout(function () {
 			popup.style.display = "none";
+			popup.style.left = "";
+			arrow.style.left = "";
 			pendingHideEventId = null;
 		}, 120);
 	});
