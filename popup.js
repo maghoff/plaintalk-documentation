2348bdf669ec7b5900f042ab86554b5de0256a80
Magnus Hoff <maghoff@gmail.com>
2013-12-13 16:27:59 +0100

    Actively toggle display for popups with javascript

--- popup.js
+++ popup.js
@@ -11,19 +11,45 @@ function createPopup(text) {
 	return element;
 }
 
+function installPopup(element) {
+	var title = extractAndRemoveTitle(element);
+	var popup = createPopup(title);
+	element.classList.add("has-popup");
+
+	var wrapper = document.createElement("span");
+	wrapper.className = "popup-wrapper";
+
+	element.parentNode.replaceChild(wrapper, element);
+	wrapper.appendChild(element);
+	wrapper.appendChild(popup);
+
+
+	popup.style.display = "none";
+	popup.classList.add("suppress-popup");
+	var pendingHideEventId = null;
+	element.addEventListener("mouseover", function () {
+		popup.style.display = "";
+		setTimeout(function () {
+			popup.classList.remove("suppress-popup");
+		}, 0);
+		if (pendingHideEventId !== null) {
+			clearTimeout(pendingHideEventId);
+			pendingHideEventId = null;
+		}
+	});
+	element.addEventListener("mouseout", function () {
+		popup.classList.add("suppress-popup");
+		pendingHideEventId = setTimeout(function () {
+			popup.style.display = "none";
+			pendingHideEventId = null;
+		}, 120);
+	});
+}
+
 function installPopups() {
 	var elements = document.querySelectorAll("span[title]");
 	for (var i = 0; i < elements.length; ++i) {
-		var title = extractAndRemoveTitle(elements[i]);
-		var popup = createPopup(title);
-		elements[i].classList.add("has-popup");
-
-		var wrapper = document.createElement("span");
-		wrapper.className = "popup-wrapper";
-
-		elements[i].parentNode.replaceChild(wrapper, elements[i]);
-		wrapper.appendChild(elements[i]);
-		wrapper.appendChild(popup);
+		installPopup(elements[i]);
 	}
 }
 
